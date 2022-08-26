93c9c0424efbb139cb9b3ab40f3abcd064028a85
Luca Barone <baro.luc@gmail.com>
2022-08-27 01:04:04 +0200

    🚧 Add dom-utils package

---
+++ packages/dom-utils/main.ts
@@ -0,0 +1,14 @@
+/**
+ * A strictier version of "document.querySelector".
+ */
+export function getBySelector(
+  ...args: Parameters<typeof document.querySelector>
+) {
+  const el = document.querySelector(...args)
+
+  if (el == null) {
+    throw Error(`${args[0]} element not found.`)
+  }
+
+  return el
+}
