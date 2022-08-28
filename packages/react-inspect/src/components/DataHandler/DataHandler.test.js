e9a0a4d3c56473a3282fec0622a5d19930cc898a
Luca Barone <baro.luc@gmail.com>
2022-08-28 03:38:52 +0200

    🐛 react-inspect: fix vulnerabilities (#9)

--- packages/react-inspect/src/components/DataHandler/DataHandler.test.js
+++ packages/react-inspect/src/components/DataHandler/DataHandler.test.js
@@ -1,50 +1,135 @@
+import {render} from '@testing-library/react'
 import React from 'react'
-import {shallow} from 'enzyme'
+import data, {arr, fun, nil, num, str} from '../../mocks/data'
 
 import Component from './index'
-import data, {arr, nil, num, fun, str} from '../../mocks/data'
-
-const subject = shallow(<Component />)
 
 /* eslint-env jest */
 describe(`${Component.displayName} component`, () => {
   it('should render properly if data is undefined', () => {
-    expect(subject).toMatchSnapshot()
+    const {container} = render(<Component />)
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <span
+          style="font-weight: bold; color: purple;"
+        >
+          undefined
+        </span>
+      </div>
+    `)
   })
 
   it('should render properly if data is null', () => {
-    subject.setProps({data: nil})
-
-    expect(subject).toMatchSnapshot()
+    const {container} = render(<Component data={nil} />)
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <span
+          style="font-weight: bold; color: purple;"
+        >
+          null
+        </span>
+      </div>
+    `)
   })
 
   it('should render properly if data is a number', () => {
-    subject.setProps({data: num})
-
-    expect(subject).toMatchSnapshot()
+    const {container} = render(<Component data={num} />)
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <span
+          style="font-weight: bold; color: orange;"
+        >
+          666
+        </span>
+      </div>
+    `)
   })
 
   it('should render properly if data is a string', () => {
-    subject.setProps({data: str})
-
-    expect(subject).toMatchSnapshot()
+    const {container} = render(<Component data={str} />)
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <span
+          style="font-weight: bold; color: green;"
+        >
+          "cawabongaaa!"
+        </span>
+      </div>
+    `)
   })
 
   it('should render properly if data is a function', () => {
-    subject.setProps({data: fun})
-
-    expect(subject).toMatchSnapshot()
+    const {container} = render(<Component data={fun} />)
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <span
+          style="cursor: pointer;"
+        >
+          <span
+            style="font-weight: bold; color: magenta;"
+          >
+            fn
+          </span>
+        </span>
+      </div>
+    `)
   })
 
   it('should render properly if data is an object', () => {
-    subject.setProps({data})
-
-    expect(subject).toMatchSnapshot()
+    const {container} = render(<Component data={data} />)
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <span>
+          <span
+            style="font-weight: bold;"
+          >
+            {
+          </span>
+          <span
+            style="cursor: pointer;"
+          >
+            <span
+              style="font-weight: bold;"
+            >
+              ...
+            </span>
+          </span>
+          <span
+            style="font-weight: bold;"
+          >
+            }
+          </span>
+        </span>
+      </div>
+    `)
   })
 
   it('should render properly if data is an array', () => {
-    subject.setProps({data: arr})
-
-    expect(subject).toMatchSnapshot()
+    const {container} = render(<Component data={arr} />)
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <span>
+          <span
+            style="font-weight: bold;"
+          >
+            [
+          </span>
+          <span
+            style="cursor: pointer;"
+          >
+            <span
+              style="font-weight: bold;"
+            >
+              ...
+            </span>
+          </span>
+          <span
+            style="font-weight: bold;"
+          >
+            ]
+          </span>
+        </span>
+      </div>
+    `)
   })
 })
--- packages/react-inspect/src/components/ReactInspect/ReactInspect.test.js
+++ packages/react-inspect/src/components/ReactInspect/ReactInspect.test.js
@@ -1,18 +1,214 @@
+import {render} from '@testing-library/react'
 import React from 'react'
-import {shallow} from 'enzyme'
 
-import Component from './index'
 import data, {circ} from '../../mocks/data'
-
-const subject = shallow(<Component data={data} />)
+import Component from './index'
 
 /* eslint-env jest */
 describe(`${Component.displayName} component`, () => {
   it('should render properly', () => {
-    expect(subject).toMatchSnapshot()
+    const {container} = render(<Component data={data} />)
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <pre
+          style="line-height: 1.25; font-size: 1.25rem; font-family: monospace; padding: 0.75rem; display: inline-block; background-color: transparent;"
+        >
+          <span>
+            <span
+              style="font-weight: bold;"
+            >
+              {
+            </span>
+            <div
+              style="padding-left: 1rem;"
+            >
+              <span
+                style="font-weight: bold; color: rgb(119, 119, 119);"
+              >
+                und
+              </span>
+              <span
+                style="font-weight: bold;"
+              >
+                :
+              </span>
+               
+              <span
+                style="font-weight: bold; color: purple;"
+              >
+                undefined
+              </span>
+            </div>
+            <div
+              style="padding-left: 1rem;"
+            >
+              <span
+                style="font-weight: bold; color: rgb(119, 119, 119);"
+              >
+                nil
+              </span>
+              <span
+                style="font-weight: bold;"
+              >
+                :
+              </span>
+               
+              <span
+                style="font-weight: bold; color: purple;"
+              >
+                null
+              </span>
+            </div>
+            <div
+              style="padding-left: 1rem;"
+            >
+              <span
+                style="font-weight: bold; color: rgb(119, 119, 119);"
+              >
+                num
+              </span>
+              <span
+                style="font-weight: bold;"
+              >
+                :
+              </span>
+               
+              <span
+                style="font-weight: bold; color: orange;"
+              >
+                666
+              </span>
+            </div>
+            <div
+              style="padding-left: 1rem;"
+            >
+              <span
+                style="font-weight: bold; color: rgb(119, 119, 119);"
+              >
+                str
+              </span>
+              <span
+                style="font-weight: bold;"
+              >
+                :
+              </span>
+               
+              <span
+                style="font-weight: bold; color: green;"
+              >
+                "cawabongaaa!"
+              </span>
+            </div>
+            <div
+              style="padding-left: 1rem;"
+            >
+              <span
+                style="font-weight: bold; color: rgb(119, 119, 119);"
+              >
+                fun
+              </span>
+              <span
+                style="font-weight: bold;"
+              >
+                :
+              </span>
+               
+              <span
+                style="cursor: pointer;"
+              >
+                <span
+                  style="font-weight: bold; color: magenta;"
+                >
+                  fn
+                </span>
+              </span>
+            </div>
+            <div
+              style="padding-left: 1rem;"
+            >
+              <span
+                style="font-weight: bold; color: rgb(119, 119, 119);"
+              >
+                arr
+              </span>
+              <span
+                style="font-weight: bold;"
+              >
+                :
+              </span>
+               
+              <span>
+                <span
+                  style="font-weight: bold;"
+                >
+                  [
+                </span>
+                <span
+                  style="cursor: pointer;"
+                >
+                  <span
+                    style="font-weight: bold;"
+                  >
+                    ...
+                  </span>
+                </span>
+                <span
+                  style="font-weight: bold;"
+                >
+                  ]
+                </span>
+              </span>
+            </div>
+            <div
+              style="padding-left: 1rem;"
+            >
+              <span
+                style="font-weight: bold; color: rgb(119, 119, 119);"
+              >
+                obj
+              </span>
+              <span
+                style="font-weight: bold;"
+              >
+                :
+              </span>
+               
+              <span>
+                <span
+                  style="font-weight: bold;"
+                >
+                  {
+                </span>
+                <span
+                  style="cursor: pointer;"
+                >
+                  <span
+                    style="font-weight: bold;"
+                  >
+                    ...
+                  </span>
+                </span>
+                <span
+                  style="font-weight: bold;"
+                >
+                  }
+                </span>
+              </span>
+            </div>
+            <span
+              style="font-weight: bold;"
+            >
+              }
+            </span>
+          </span>
+        </pre>
+      </div>
+    `)
   })
 
   it('should throw an error if the data is circular', () => {
-    expect(() => subject.setProps({data: circ})).toThrowError()
+    jest.spyOn(console, 'error').mockImplementation(jest.fn)
+    expect(() => render(<Component data={circ} />)).toThrowError()
+    jest.restoreAllMocks()
   })
 })
