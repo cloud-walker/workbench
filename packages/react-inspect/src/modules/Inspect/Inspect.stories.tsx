56a87f19d2475dcb1a13eb3ecd18887cee33f02e
Luca Barone <baro.luc@gmail.com>
2022-09-04 23:44:15 +0200

    🐛 Fix react-inspect stories

--- packages/react-inspect/src/modules/Inspect/Inspect.stories.tsx
+++ packages/react-inspect/src/modules/Inspect/Inspect.stories.tsx
@@ -1,6 +1,6 @@
 import {ComponentMeta} from '@storybook/react'
 import {Inspect} from '.'
-import data from '../testUtils'
+import {data} from '../testUtils'
 
 const meta: ComponentMeta<typeof Inspect> = {
   title: 'Inspect',
