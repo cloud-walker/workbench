7cb5606bd2a18ea19e35f2a6fffacef5feeca3dd
Luca Barone <baro.luc@gmail.com>
2022-09-11 23:48:46 +0200

    🎉 Add react filters-bar module

---
+++ packages/react-filters-bar/src/filter-context.tsx
@@ -0,0 +1,26 @@
+import { createContext, PropsWithChildren, useContext } from "react";
+
+type FilterState = {
+  remove: () => void;
+  setOperator: (value: string) => void;
+  setValue: (value: string) => void;
+  operator: string;
+  value: string;
+};
+
+const FilterCtx = createContext<FilterState | null>(null);
+
+export function useFilterCtx() {
+  const ctx = useContext(FilterCtx);
+  if (ctx == null) {
+    throw Error("FilterCtx.Provider is missing.");
+  }
+  return ctx;
+}
+
+export function FilterProvider({
+  children,
+  ...props
+}: PropsWithChildren<Provider>) {
+  return <FilterCtx.Provider value={props}>{children}</FilterCtx.Provider>;
+}
