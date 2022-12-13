fd93e13dea4bfdcee7d39dd1cd71f8dc78f5b398
Luca Barone <baro.luc@gmail.com>
2022-12-13 09:11:20 +0100

    wip

--- packages/items-encoder/src/main.test.ts
+++ packages/items-encoder/src/main.test.ts
@@ -1,5 +1,9 @@
 import {expect, test} from 'vitest'
-import {decodeResultsSortings, encodeResultsSortings} from './main'
+import {
+  decodeResultsSortings,
+  encodeResultsSortings,
+  makeResultsSortingsEncoder,
+} from './main'
 
 test('encode results sortings', () => {
   expect(
@@ -18,4 +22,48 @@ test('decode results sortings', () => {
     {field: 'bar', operator: 'desc'},
     {field: 'baz', operator: 'asc'},
   ])
+})
+
+test('tasks results sortings encode / decode', () => {
+  type Task = {
+    id: string
+    content: string
+    categories: string[]
+    isCompleted: boolean
+    estimate: number
+    createdAt: Date
+    updatedAt: Date
+  }
+  const encoder = makeResultsSortingsEncoder<Record<keyof Task, string>>({
+    categories: 'cg',
+    content: 'cn',
+    createdAt: 'ca',
+    estimate: 'es',
+    id: 'id',
+    isCompleted: 'ic',
+    updatedAt: 'ua',
+  })
+
+  {
+    const decoded = encoder.decode('')
+    expect(decoded).toEqual([])
+  }
+
+  {
+    const decoded = encoder.decode('id:a,cg:d,es:a')
+    expect(decoded).toEqual([
+      {field: 'id', operator: 'asc'},
+      {field: 'categories', operator: 'desc'},
+      {field: 'estimate', operator: 'asc'},
+    ])
+  }
+
+  expect(encoder.encode([])).toBe('')
+  expect(
+    encoder.encode([
+      {field: 'id', operator: 'asc'},
+      {field: 'categories', operator: 'desc'},
+      {field: 'estimate', operator: 'asc'},
+    ]),
+  ).toBe('id:a,cg:d,es:a')
 })
--- packages/items-encoder/src/main.ts
+++ packages/items-encoder/src/main.ts
@@ -1,7 +1,10 @@
 import {pipe, reduce} from 'remeda'
 import {z} from 'zod'
 
-type ResultSorting = {field: string; operator: 'asc' | 'desc'}
+type ResultSorting<TField extends string = string> = {
+  field: TField
+  operator: 'asc' | 'desc'
+}
 
 const encodedOperatorSchema = z.union([
   z.literal('a').transform(() => 'asc' as const),
@@ -43,9 +46,58 @@ export function decodeResultsSortings(data: string): ResultSorting[] {
   })
 }
 
-export function makeResultsSortingsEncoder() {
-  const encode = () => {}
-  const decode = () => {}
+export function makeResultsSortingsEncoder<
+  TFieldsMap extends Readonly<Record<string, string>>,
+>(fieldsMap: TFieldsMap) {
+  type Field = Extract<keyof TFieldsMap, string>
+  const encodings = Object.values(fieldsMap) as unknown as ReadonlyArray<
+    TFieldsMap[keyof TFieldsMap]
+  >
+  const fieldsBiMap = makeBiMap(fieldsMap)
+  const encode = (data: ResultSorting<Field>[]): string =>
+    data.reduce((acc, x) => {
+      const frag = `${fieldsBiMap.value(x.field)}:${decodedOperatorSchema.parse(
+        x.operator,
+      )}`
+      if (acc == '') {
+        return frag
+      }
+      return `${acc},${frag}`
+    }, '')
+  const decode = (data: string): ResultSorting<Field>[] => {
+    if (data == '') {
+      return []
+    }
+    return data.split(',').map((s) => {
+      const [_field, _operator] = s.split(':')
+      const operator = encodedOperatorSchema.parse(_operator)
+      const field = fieldsBiMap.key(
+        z
+          .enum(
+            /** @ts-ignore-next */
+            encodings,
+          )
+          .parse(_field) as TFieldsMap[Field],
+      ) as Field
+      return {
+        field,
+        operator,
+      }
+    })
+  }
 
   return {encode, decode} as const
+}
+
+function makeBiMap<TData extends Readonly<Record<string, string>>>(
+  data: TData,
+) {
+  type Key = Extract<keyof TData, string>
+  type Value = TData[Key]
+  const inverseData = Object.fromEntries(
+    Object.entries(data).map(([a, b]) => [b, a]),
+  ) as {[k in keyof TData as TData[k]]: k}
+  const key = (value: Value) => inverseData[value]
+  const value = (key: Key) => data[key]
+  return {key, value} as const
 }
